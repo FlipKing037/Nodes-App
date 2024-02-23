50746950b20e42031880742a63eb63468fa5ba3c
Phillip Fawzy <flipking037@gmail.com>
2024-02-23 16:40:38 +0200

    add custom add button

--- lib/views/widget/add_show_modal_bottom_sheet.dart
+++ lib/views/widget/add_show_modal_bottom_sheet.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/widget/custom_add_buttom.dart';
 import 'package:notes_app/views/widget/custom_text_field.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
@@ -8,22 +9,28 @@ class AddNoteBottomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Padding(
       padding: EdgeInsets.symmetric(horizontal: 16),
-      child: Column(
-        children: [
-          SizedBox(
-            height: 32,
-          ),
-          CustomTextField(
-            hintText: 'Title',
-          ),
-          SizedBox(
-            height: 16,
-          ),
-          CustomTextField(
-            hintText: 'Content',
-            maxlines: 5,
-          ),
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 32,
+            ),
+            CustomTextField(
+              hintText: 'Title',
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            CustomTextField(
+              hintText: 'Content',
+              maxlines: 5,
+            ),
+            SizedBox(
+              height: 32,
+            ),
+            CustomAddButton(),
+          ],
+        ),
       ),
     );
   }
