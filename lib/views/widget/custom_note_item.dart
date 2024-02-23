c09f72f49b56091f7baf7d05be37bae24125f6d3
Phillip Fawzy <flipking037@gmail.com>
2024-02-23 17:40:25 +0200

    add edit note view

--- lib/views/widget/custom_note_item.dart
+++ lib/views/widget/custom_note_item.dart
@@ -1,57 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/edit_note_view.dart';
 
 class CustomNoteItem extends StatelessWidget {
   const CustomNoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
-      decoration: BoxDecoration(
-        color: const Color(0xffffcd7a),
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: const Text(
-              'Flutter Tips',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 26,
-              ),
-            ),
-            subtitle: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 16),
-              child: Text(
-                'Bullder Your note here offline user',
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) {
+            return const EditNoteView();
+          }),
+        );
+      },
+      child: Container(
+        padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
+        decoration: BoxDecoration(
+          color: const Color(0xffffcd7a),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: const Text(
+                'Flutter Tips',
                 style: TextStyle(
-                  color: Colors.black.withOpacity(0.5),
-                  fontSize: 18,
+                  color: Colors.black,
+                  fontSize: 26,
                 ),
               ),
-            ),
-            trailing: IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                Icons.delete,
-                size: 32,
+              subtitle: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16),
+                child: Text(
+                  'Bullder Your note here offline user',
+                  style: TextStyle(
+                    color: Colors.black.withOpacity(0.5),
+                    fontSize: 18,
+                  ),
+                ),
               ),
-              color: Colors.black,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 18),
-            child: Text(
-              'may21,2022',
-              style: TextStyle(
-                color: Colors.black.withOpacity(0.4),
-                fontSize: 16,
+              trailing: IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.delete,
+                  size: 32,
+                ),
+                color: Colors.black,
               ),
             ),
-          )
-        ],
+            Padding(
+              padding: const EdgeInsets.only(right: 18),
+              child: Text(
+                'may21,2022',
+                style: TextStyle(
+                  color: Colors.black.withOpacity(0.4),
+                  fontSize: 16,
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
